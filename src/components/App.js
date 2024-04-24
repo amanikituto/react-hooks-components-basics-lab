641fac1e978a26cfb3dc1652850a967bcf25ba02
amanikitutocd.. <akituto@gmail.com>
2024-04-24 07:44:51 +0300

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -8,29 +8,47 @@ function NavBar() {
   );
 }
 
-function Home() {
+function Home(props) {
   return (
     <div id="home">
-      <h1>Home</h1>
+      <h1>Welcome, {props.user.name}!</h1>
     </div>
   );
 }
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About</h2>
+      {props.bio && <p>{props.bio}</p>}
+      <Links github={props.github} linkedin={props.linkedin} />
     </div>
+  );
+}
+
+function Links(props) {
+  return (
+    <nav>
+      <a href={props.github}>GitHub</a>
+      <a href={props.linkedin}>LinkedIn</a>
+    </nav>
   );
 }
 
 
 function App() {
+  const user = {
+    name: 'John Doe',
+    bio: 'This is a short bio about John Doe.',
+    github: 'https://github.com/johndoe',
+    linkedin: 'https://linkedin.com/in/johndoe'
+  };
+
   return (
     <div>
       <NavBar />
-      <Home/>
-      <About/>
+      <Home user={user} />
+      <About bio={user.bio} github={user.github} linkedin={user.linkedin} />
     </div>
   );
 }
